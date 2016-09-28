4ac9d963df5aaa7c5355a79eabf4d776e0ea680b
ckib16 <ckib16@gmail.com>
2016-09-28 14:09:23 -0400

    finished methods

---
+++ 100_backend_prep/methods.rb
@@ -0,0 +1,22 @@
+def greeting(name)
+  puts "Hello #{name}!"
+end
+
+greeting("Chris")
+
+############
+
+def multiply(a, b)
+  puts a*b
+end
+
+multiply(2,3)
+
+#
+
+def scream(words)
+  words =  words + "!!!"
+  puts words
+end
+
+scream("Yippeee")
---
+++ 100_backend_prep/say.rb
@@ -0,0 +1,9 @@
+def say(words= 'hello')
+  puts words + '.'
+end
+
+say()
+say("yo")
+say("hi")
+say("how are you")
+say("I'm fine")
