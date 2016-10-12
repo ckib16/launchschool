690f3cfe250b9f215f0db2e5e38d325ae9981ac1
ckib16 <ckib16@gmail.com>
2016-10-12 13:05:38 -0400

    psuedo code ex

---
+++ 101_foundations/l2/psuedo_code.rb
@@ -0,0 +1,13 @@
+GIVEN two integers
+Add 1st & second integers
+Print result
+
+GIVEN an array of strings
+Use the join method to add strings
+Print strings concatenated
+
+GIVEN an array of integers
+Use map to return a new array
+See if iterator is odd using iterator%2 != 0
+Select only odd elements
+return new array of odd elements
