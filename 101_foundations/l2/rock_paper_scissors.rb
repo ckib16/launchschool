2065b17a5645a264651fd4811d6c0198ef6c9bdf
ckib16 <ckib16@gmail.com>
2016-10-21 18:23:48 -0400

    stuck on counter resetting

--- 101_foundations/l2/rock_paper_scissors.rb
+++ 101_foundations/l2/rock_paper_scissors.rb
@@ -1,7 +1,24 @@
+require 'pry'
+
 VALID_CHOICES = %w(rock paper scissors lizard spock)
+total_wins = 0
 
 def prompt(message)
   puts "=> #{message}"
+end
+
+def autocomplete_player_choice(choice)
+  if choice.start_with?('r')
+    'rock'
+  elsif choice.start_with?('p')
+    'paper'
+  elsif choice.start_with?('sc')
+    'scissors'
+  elsif choice.start_with?('l')
+    'lizard'
+  elsif choice.start_with?('sp')
+    'spock'
+  end
 end
 
 def win?(first, second)
@@ -12,11 +29,14 @@ def win?(first, second)
     (first == 'spock' && second.start_with?('sc', 'r'))
 end
 
-def display_results(player, computer)
+def display_results(player, computer, total_wins)
   if win?(player, computer)
     prompt("You win!")
+    total_wins += 1
+    # binding.pry
   elsif win?(computer, player)
     prompt("You lose!")
+    total_wins -= 1
   else
     prompt("It's a tie.")
   end
@@ -26,11 +46,14 @@ prompt("Welome to Rock, Paper, Scissors, Lizard, Spock")
 
 loop do
   player_choice = ''
+
   loop do
     prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    player_choice = gets.chomp
+    prompt("You can just put the 1st two letters: Ex - 'sp' = Spock")
+    player_choice = gets.downcase.chomp
 
-    if VALID_CHOICES.include?(player_choice)
+    if player_choice.start_with?('r', 'p', 'sc', 'l', 'sp')
+      player_choice = autocomplete_player_choice(player_choice)
       break
     else
       prompt("That's not a valid choice.")
@@ -41,7 +64,8 @@ loop do
 
   prompt("You chose #{player_choice}. Computer chose #{computer_choice}.")
 
-  display_results(player_choice, computer_choice)
+  display_results(player_choice, computer_choice, total_wins)
+  prompt("Your total wins are #{total_wins}")
 
   prompt("Want to play again? Press 'y' if Yes.")
   play_again = gets.chomp
