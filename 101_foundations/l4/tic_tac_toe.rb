bfbfc236d29e767d1d2d04d4fe6b1f5500b3cf83
ckib16 <ckib16@gmail.com>
2016-10-27 09:17:52 -0400

    main loop

--- 101_foundations/l4/tic_tac_toe.rb
+++ 101_foundations/l4/tic_tac_toe.rb
@@ -18,17 +18,18 @@ def prompt(msg)
 end
 
 def display_board(brd)
+  system 'clear'
   puts ""
   puts "     |     |     "
-  puts "  #{brd[1]}  |  #{brd[2]}  |   #{brd[3]}  "
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}   "
   puts "     |     |     "
   puts "-----|-----|-----"
   puts "     |     |     "
-  puts "  #{brd[4]}  |  #{brd[5]}  |   #{brd[6]}  "
+  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}   "
   puts "     |     |     "
   puts "-----|-----|-----"
   puts "     |     |     "
-  puts "  #{brd[7]}  |  #{brd[8]}  |   #{brd[9]}  "
+  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}   "
   puts "     |     |     "
   puts ""
 end
@@ -55,9 +56,27 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
+def computer_places_pieces!(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
+
+def someone_won?(brd)
+  false
+end
+
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
 board = initialize_board
 display_board(board)
 
-player_places_piece!(board)
-puts board.inspect
+loop do
+  player_places_piece!(board)
+  computer_places_pieces!(board)
+  display_board(board)
+  break if someone_won?(board) || board_full?(board)
+end
+
 display_board(board)
