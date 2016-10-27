30d4b6ee69981d7c939200a0a37f555fb61e0d0f
ckib16 <ckib16@gmail.com>
2016-10-27 10:40:17 -0400

    initial tic tac toe complete

--- 101_foundations/l4/tic_tac_toe.rb
+++ 101_foundations/l4/tic_tac_toe.rb
@@ -19,6 +19,8 @@ end
 
 def display_board(brd)
   system 'clear'
+  puts "Player   = #{PLAYER_MARKER}"
+  puts "Computer = #{COMPUTER_MARKER}"
   puts ""
   puts "     |     |     "
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}   "
@@ -69,14 +71,52 @@ def board_full?(brd)
   empty_squares(brd).empty?
 end
 
-board = initialize_board
-display_board(board)
-
-loop do
-  player_places_piece!(board)
-  computer_places_pieces!(board)
-  display_board(board)
-  break if someone_won?(board) || board_full?(board)
+def someone_won?(brd)
+  !!detect_winner(brd)
 end
 
-display_board(board)
+def detect_winner(brd)
+  winning_lines = [[1 ,2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
+                  [[1, 5, 9], [3, 5, 7]] # diagonals
+  winning_lines.each do |line|
+    if brd[line[0]] == PLAYER_MARKER &&
+       brd[line[1]] == PLAYER_MARKER &&
+       brd[line[2]] == PLAYER_MARKER
+       return 'Player'
+     elsif brd[line[0]] == COMPUTER_MARKER &&
+       brd[line[1]] == COMPUTER_MARKER &&
+       brd[line[2]] == COMPUTER_MARKER
+       return 'Computer'
+    end
+  end
+  nil
+end
+
+loop do
+board = initialize_board
+
+  loop do
+    display_board(board)
+
+    player_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+
+    computer_places_pieces!(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+
+  display_board(board)
+
+  if someone_won?(board)
+    prompt "#{detect_winner(board)} won!"
+  else
+    prompt "It's a tie."
+  end
+
+  prompt "Do you want to play again? (y)"
+  play_again = gets.chomp.downcase
+  break unless play_again.start_with?('y')
+end
+
+prompt "Goodbye. Thanks for playing."
