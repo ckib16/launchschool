b54c3e8bf0875cbc3345904dffc820a4764119bc
ckib16 <ckib16@gmail.com>
2016-11-17 07:44:42 -0500

    added context to when counter works

--- 101_foundations/l4/counter_loop.rb
+++ 101_foundations/l4/counter_loop.rb
@@ -11,13 +11,13 @@ loop do
 
   if input.start_with?('y')
 
-    # works with brute force, no method used to increment
+    # INCREMENT WORKS: brute force, no method used to increment
     # counter += 1
 
-    # doesn't work because not ASSIGNING the method's returned value to the local variable. This just returns a new number - 1, 2, 3 etc. - but DOESN'T mutate `counter` to "hold" or "contain" or be assigned that new number.
+    # DOES NOT WORK: we don't save/assign the method's returned value to a local variable for later use. This just returns a new number - 1, 2, 3 etc. - but DOESN'T mutate `counter` to "hold" or "contain" or be assigned that new number.
     # increment(counter)
 
-    # works b/c we **ASSIGN THE LOCAL VARIABLE TO THE RETURN VALUE OF THE METHOD** so that the local variable is mutated/updated. This ensures the method does its job (whatever action it is) and we guaantee that the local variable is mutated as well be assigning it outside of the method.
+    #  INCREMENT WORKS: we **ASSIGN THE LOCAL VARIABLE TO THE RETURN VALUE OF THE METHOD** so that the local variable is mutated/updated. This ensures the method does its job (whatever action it is) and we guaantee that the local variable is mutated as well be assigning it outside of the method.
     counter = increment(counter)
 
   elsif input.start_with?('q')
